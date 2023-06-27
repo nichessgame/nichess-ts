0172ab1203355cbee57ff019340afb084967be0c
vasilije2448 <73365189+vasilije2448@users.noreply.github.com>
2023-06-27 16:48:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Download and install Node.js and npm https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+
+To install typescript and tslib run:
+
+```
+npm install
+```
+
+To compile run:
+
+```
+npm run compile
+```
---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+export const NUM_ROWS = 8;
+export const NUM_COLUMNS = 8;
+export const NUM_STARTING_PIECES = 7;
+export const ABILITY_SKIP = -1;
+export const MOVE_SKIP = -1;
+
+export const KING_STARTING_HEALTH_POINTS = 200;
+export const MAGE_STARTING_HEALTH_POINTS = 230;
+export const PAWN_STARTING_HEALTH_POINTS = 300;
+export const WARRIOR_STARTING_HEALTH_POINTS = 500;
+export const ASSASSIN_STARTING_HEALTH_POINTS = 110;
+
+export const KING_ABILITY_POINTS = 60;
+export const MAGE_ABILITY_POINTS = 80;
+export const PAWN_ABILITY_POINTS = 30;
+export const WARRIOR_ABILITY_POINTS = 100;
+export const ASSASSIN_ABILITY_POINTS = 120;
+
+export const NUM_PLAYERS = 2;
+export const NUM_PIECE_TYPE = 11;
+
+// piece index is not the same thing as board(square) index
+// it is used as an array index for faster access to a specific piece
+export const MAGE_PIECE_INDEX = 0;
+export const ASSASSIN_PIECE_INDEX = 1;
+export const WARRIOR_PIECE_INDEX = 2;
+export const PAWN_1_PIECE_INDEX = 3;
+export const PAWN_2_PIECE_INDEX = 4;
+export const PAWN_3_PIECE_INDEX = 5;
+export const KING_PIECE_INDEX = 6;
+
